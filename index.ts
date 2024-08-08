46f49f3144b01450f84715ce6b32b24dd43f13c9
YonatanQuintero <yonatan.a.quintero.r@gmail.com>
2024-08-08 17:08:56 -0500

    remove v1

--- index.ts
+++ index.ts
@@ -1,12 +1,11 @@
 import { getTronWeb, NILE_USDT_CONTRACT, USER_ADDRESS } from "./config";
-import { CalculateResources } from "./src/calculate-resources";
 import { estimateResources } from "./src/v2/estimate-resources";
 import { resourcesDelegation } from "./src/v2/resources-delegation";
 import { sendUsdt } from "./src/v2/send-usdt";
 
 const main = async () => {
-    // await estimateResources();
-    await sendUsdt();
+    await estimateResources();
+    // await sendUsdt();
     // await resourcesDelegation();
 }
 
